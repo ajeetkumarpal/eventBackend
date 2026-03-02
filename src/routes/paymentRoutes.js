e3ddae08db90c863abe9d66046e6dfcf04704e4f
Ajeet kumar pal <ajeetkumarpal22@gmail.com>
2026-03-02 22:45:51 +0530

    jjj

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { paymentController, verifyPayment } from "../controller/rozarpayController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const paymentRouter=express.Router();
 
-paymentRouter.post("/payment-order", paymentController);
-paymentRouter.post("/payment-verify",verifyPayment);
+paymentRouter.post("/payment-order",userAuth,paymentController);
+paymentRouter.post("/payment-verify",userAuth,verifyPayment);
 
 export default paymentRouter;
